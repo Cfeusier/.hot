f6efd83acf80fc61fa068975e35988ecccfdb2e3
Clark Feusier <cfeusier@gmail.com>
2017-07-07 00:49:03 -0700

    Add install script for testing

--- .env/.bin/install.sh
+++ .env/.bin/install.sh
@@ -1,24 +1,24 @@
-##!/usr/bin/env bash
-#
-#git clone --bare git@github.com:Cfeusier/.ctf.git $HOME/.ctf
-#
-#ctf() {
-#  /usr/bin/git --git-dir=$HOME/.ctf/ --work-tree=$HOME $@
-#}
-#
-#mkdir -p .ctf-backup
-#
-#ctf checkout
-#
-#if [$? = 0]; then
-#  echo "Checked out .ctf/";
-#else
-#  echo "Backing up pre-existing ./ctf";
-#  ctf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .ctf-backup/{}
-#fi;
-#
-#ctf checkout
-#ctf config status.showUntrackedFiles no
+#!/usr/bin/env bash
+
+git clone --bare git@github.com:Cfeusier/.ctf.git $HOME/.ctf
+
+ctf() {
+  /usr/bin/git --git-dir=$HOME/.ctf/ --work-tree=$HOME $@
+}
+
+mkdir -p .ctf-backup
+
+ctf checkout
+
+if [$? = 0]; then
+  echo "Checked out .ctf/";
+else
+  echo "Backing up pre-existing ./ctf";
+  ctf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .ctf-backup/{}
+fi;
+
+ctf checkout
+ctf config status.showUntrackedFiles no
 
 source $HOME/.env/.bin/link.sh
 
