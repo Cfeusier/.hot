5e2b729445a5e882142abc997fc3b4fc6d3f8719
Clark Feusier <clark@clark.local>
2017-07-07 21:00:30 -0700

    Move clean into install

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env bash
 
+echo 'Removing .ctf/ before cloning repository...'
+rm -rf $HOME/.ctf
+echo 'Done removing .ctf/'
+echo 'Removing .env/...'
+rm -rf $HOME/.env
+echo 'Done removing .env/'
+echo 'Removing .bin/...'
+rm -rf $HOME/.bin
+echo 'Done removing .bin/'
+echo 'Removing .bashrc...'
+rm -rf $HOME/.bashrc
+echo 'Done removing .bashrc'
+echo 'Removing .bash_profile...'
+rm -rf $HOME/.bash_profile
+echo 'Done removing .bash_profile'
+echo 'Removing .zshrc...'
+rm -rf $HOME/.zshrc
+echo 'Done removing .zshrc'
+echo 'Removing .tmux.conf...'
+rm -rf $HOME/.tmux.conf
+echo 'Done removing .tmux.conf'
+echo 'Removing .vimrc...'
+rm -rf $HOME/.vimrc
+echo 'Done removing .vimrc'
+echo 'Removing .gitconfig...'
+rm -rf $HOME/.gitconfig
+echo 'Done removing .gitconfig'
+echo 'Removing .gitignore...'
+rm -rf $HOME/.gitignore
+echo 'Done removing .gitignore'
+echo 'Removing .gitignore_global'
+rm -rf $HOME/.gitignore_global
+echo 'Done removing .gitignore_global'
+
 git clone --bare git@github.com:Cfeusier/.ctf.git $HOME/.ctf
 
 ctf() {
@@ -7,9 +41,9 @@ ctf() {
 }
 
 # mkdir -p .ctf-backup
-# 
+#
 # ctf checkout
-# 
+#
 # if [ $? = 0 ]; then
 #   echo "Checked out .ctf/";
 # else
@@ -20,9 +54,6 @@ ctf() {
 ctf checkout
 
 ctf config status.showUntrackedFiles no
-
-source $HOME/.bin/clean.sh
-clean
 
 source $HOME/.bin/link.sh
 linkAll
