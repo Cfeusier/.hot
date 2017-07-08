7513d709a943551181d912eec18fcd0974720fd4
Clark Feusier <cfeusier@gmail.com>
2017-07-08 15:41:21 -0700

    Remove directory . prefix

--- .bin/link.sh
+++ .bin/link.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 linkBash() {
-  ln -s .env/.bash/.bashrc ~/.bashrc
-  ln -s .env/.bash/.bash_profile ~/.bash_profile
+  ln -s .env/bash/.bashrc ~/.bashrc
+  ln -s .env/bash/.bash_profile ~/.bash_profile
 }
 
 linkZsh() {
-  ln -s .env/.zsh/.zshrc ~/.zshrc
+  ln -s .env/zsh/.zshrc ~/.zshrc
 }
 
 linkTmux() {
-  ln -s .env/.tmux/.tmux.conf ~/.tmux.conf
+  ln -s .env/tmux/.tmux.conf ~/.tmux.conf
 }
 
 linkVim() {
-  ln -s .env/.vim/.vimrc ~/.vimrc
+  ln -s .env/vim/.vimrc ~/.vimrc
 }
 
 linkGit() {
   ln -s .env/git/.gitconfig ~/.gitconfig
-  ln -s .env/.git/.gitignore ~/.gitignore
+  ln -s .env/git/.gitignore ~/.gitignore
 }
 
 linkAll() {
