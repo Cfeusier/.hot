f56b2ccb5a0c942419d15ad3933d9793d1e502eb
Clark Feusier <cfeusier@gmail.com>
2017-07-08 15:51:41 -0700

    Add log statements

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,26 +2,37 @@
 
 # Cleanup before install: since this is for new instances only, no backup needed #
 # CAUTION: DO NOT RUN THIS unless you are ok with the dotfiles listed below getting nuked #
+echo '-------------------------------------------'
 echo 'Removing .ctf/ before cloning repository...'
 rm -rf $HOME/.ctf
+echo '-------------------------------------------'
 echo 'Removing .env/...'
 rm -rf $HOME/.env
+echo '-------------------------------------------'
 echo 'Removing .bin/...'
 rm -rf $HOME/.bin
+echo '-------------------------------------------'
 echo 'Removing .bashrc...'
 rm $HOME/.bashrc
+echo '-------------------------------------------'
 echo 'Removing .bash_profile...'
 rm $HOME/.bash_profile
+echo '-------------------------------------------'
 echo 'Removing .zshrc...'
 rm $HOME/.zshrc
+echo '-------------------------------------------'
 echo 'Removing .tmux.conf...'
 rm $HOME/.tmux.conf
+echo '-------------------------------------------'
 echo 'Removing .vimrc...'
 rm $HOME/.vimrc
+echo '-------------------------------------------'
 echo 'Removing .gitconfig...'
 rm $HOME/.gitconfig
+echo '-------------------------------------------'
 echo 'Removing .gitignore...'
 rm $HOME/.gitignore
+echo '-------------------------------------------'
 
 git clone --bare git@github.com:Cfeusier/.ctf.git $HOME/.ctf
 
@@ -29,9 +40,12 @@ ctf() {
   /usr/bin/git --git-dir=$HOME/.ctf/ --work-tree=$HOME $@
 }
 
+echo '-------------------------------------------'
 ctf checkout
 ctf config status.showUntrackedFiles no
 
+echo '-------------------------------------------'
+echo 'Linking repository to relevant dotfiles...'
 source $HOME/.bin/link.sh
 linkAll
 
--- .bin/link.sh
+++ .bin/link.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
 linkBash() {
+  echo 'Linking .env/bash/.bashrc to ~/.bashrc...'
   ln -s .env/bash/.bashrc ~/.bashrc
+  echo 'Linking .env/bash/.bash_profile to ~/.bash_profile...'
   ln -s .env/bash/.bash_profile ~/.bash_profile
 }
 
 linkZsh() {
+  echo 'Linking .env/zsh/.zshrc to ~/.zshrc...'
   ln -s .env/zsh/.zshrc ~/.zshrc
 }
 
 linkTmux() {
+  echo 'Linking .env/tmux/.tmux.conf to ~/.tmux.conf...'
   ln -s .env/tmux/.tmux.conf ~/.tmux.conf
 }
 
 linkVim() {
+  echo 'Linking .env/vim/.vimrc to ~/.vimrc...'
   ln -s .env/vim/.vimrc ~/.vimrc
 }
 
 linkGit() {
+  echo 'Linking .env/git/.gitconfig to ~/.gitconfig...'
   ln -s .env/git/.gitconfig ~/.gitconfig
+  echo 'Linking .env/git/.gitignore to ~/.gitignore...'
   ln -s .env/git/.gitignore ~/.gitignore
 }
 
